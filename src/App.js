45c686c3711b469e59125fb6f556fec7c3d06a3c
haluskua <haluskua.one@gmail.com>
2022-01-04 12:37:01 +1300

    added css for tables from w3c schools with destructrured data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import './App.css';
+import  BasicTable from '../src/components/BasicTable'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>hellow world</h1>
-      </header>
-    </div>
+    <React.Fragment>
+       <BasicTable />
+    </React.Fragment>
   );
 }
 
